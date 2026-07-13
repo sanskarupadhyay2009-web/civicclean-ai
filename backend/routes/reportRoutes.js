945f6b20808a021553a519ff46ec6e6f25732dca
sanskarupadhyay2009-web <sanskarupadhyay.2009@gmail.com>
2026-07-13 16:18:01 +0530

    Update reportRoutes.js

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
 const upload = require("../middleware/upload");
 const protect = require("../middleware/authMiddleware");
-const { analyzeReport } = require("../controllers/reportController");
+
+const {
+  analyzeReport,
+  getReports,
+} = require("../controllers/reportController");
 
 const router = express.Router();
 
 router.post("/analyze", protect, upload.single("image"), analyzeReport);
+
+// New route
+router.get("/", protect, getReports);
 
 module.exports = router;
