255b6043eab1e294923b5e9a746cd3dfcafb0f92
sanskarupadhyay2009-web <sanskarupadhyay.2009@gmail.com>
2026-07-13 16:46:29 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import router from "./router";
 import LoadingScreen from "./components/common/LoadingScreen";
+import CustomCursor from "./components/common/CustomCursor";
+import "./styles/theme.css";
+import "./styles/cursor.css";
 import "./styles/Global.css";
 import "./styles/auth.css";
 import "./styles/loadingscreen.css";
@@ -11,6 +14,7 @@ import "./styles/loadingscreen.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
+      <CustomCursor />
       <LoadingScreen />
       <RouterProvider router={router} />
     </AuthProvider>
